21dfed1aecfbb8e458b42fdaf7dc9fda695f7f64
Tanner Townsend <rsmcwow@gmail.com>
2022-11-07 14:13:32 -0500

    Add 8KYU sum-of-positive

---
+++ codewars/8kyu/sum-of-positive.js
@@ -0,0 +1,18 @@
+// Title: Sum of positive
+
+/** Description:
+You get an array of numbers, return the sum of all of the positives ones.
+
+Example [1,-4,7,12] => 1 + 7 + 12 = 20
+
+Note: if there is nothing to sum, the sum is default to 0.
+ */
+
+// My Solution
+function positiveSum(arr) {
+  let sum = 0;
+  arr.forEach((el) => {
+    if (el > 0) sum += el;
+  });
+  return sum;
+}
